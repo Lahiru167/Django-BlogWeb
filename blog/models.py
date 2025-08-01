e8d0b030a10d7536535a26822a4f04ca2cf32c68
Lahiru167 <lahirushehan2002@gmail.com>
2025-08-01 23:35:33 +0530

    complete the project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class blogTable(models.Model):
+    name = models.CharField(max_length=200)
+    discription =models.TextField(null=True, blank=True)
+    image = models.ImageField(default='Screenshot (3).png',null=True ,blank=True)
+    image1 = models.ImageField(default='Screenshot (3).png',null=True ,blank=True)
+    image2 = models.ImageField(default='Screenshot (3).png',null=True ,blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    
+    def __str__(self):
+        return self.name
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import blogTable
 
 def home(req):
-    return render(req,"blog.html")
+    database = blogTable.objects.all()
+    
+    return render(req,"blog.html",{"database": database})
+
+def content(req,pk):
+    database = blogTable.objects.get(id = pk)
+    return render(req,"content.html",{"db":database})
 
 # Create your views here.
 
